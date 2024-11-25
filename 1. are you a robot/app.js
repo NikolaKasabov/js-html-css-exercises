7a0f3b8384cb550cc79c828dbf7c6e6cbb1cb14a
NikolaKasabov <n_kasabov@mail.bg>
2024-11-25 11:55:26 +0200

    add generate color buttons

--- 1. are you a robot/app.js
+++ 1. are you a robot/app.js
@@ -15,5 +15,25 @@ function randomHexColor() {
 }
 
 function generateColorButtons() {
+  const randomIndex = Math.floor(Math.random() * 9);
 
+  for (let i = 0; i < 9; i++) {
+    const currentColor = randomHexColor();
+    
+    // choose random correct color
+    if (randomIndex === i) {
+      correctColor = currentColor;
+      colorPlaceholder.innerText = currentColor;
+    }
+
+    const button = document.createElement('button');
+    button.setAttribute('data-color', currentColor);
+    button.style.backgroundColor = currentColor;
+    button.innerText = currentColor;
+    colorsContainer.append(button);
+  }
 }
+
+
+
+generateColorButtons();
